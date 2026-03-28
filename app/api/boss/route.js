f0c0b72a623dc5b51753b16cdbc773d38e9ebec9
dmshr <dimass.heru@gmail.com>
2026-03-28 19:22:10 +0700

    js3 boss

--- app/api/boss/route.js
+++ app/api/boss/route.js
@@ -1,5 +1,8 @@
 import { saveBoss, getAllBosses } from "@/repositories/bossRepository";
 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const bosses = await getAllBosses();
   return Response.json(bosses);
